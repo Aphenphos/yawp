b19f789bc203f226c4c2e863ba2753ab6b99d6d6
Aphenphos <MaxwillWinters@gmail.com>
2022-08-15 13:25:27 -0400

    more setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,15 @@
 -- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+-- The SQL in this file will be executed when you run `npm run setup-db`\
+drop table if exists users;
+drop table if exists restaurants;
+drop table if exists restaurants_reviews;
+
 create table users (
     id bigint generated always as identity primary key,
     first_name text not null,
     last_name text not null,
     email text not null,
-    password_hash text not null
+    -- password_hash text not null // if doing oauth bonus
 );
 
 create table restaurants (
@@ -19,4 +23,23 @@ create table restaurants_reviews (
     star_rating int not null,
     foreign key (restaurant_id) references restaurants(id),
     foreign key (user_id) references users(id)
+);
+
+insert into users (
+    first_name,
+    last_name,
+    email
 )
+values
+    ('Squilliam', 'Fancyson', 'Squil@fancy.com'),
+    ('Billiam', 'Smith', 'Billiam@smith.com')
+;
+
+insert into restaurants (
+    name,
+    food_type
+)
+values
+    ('Mcdonalds', 'McChickens'),
+    ('Taco Bell', 'Taco')
+;
