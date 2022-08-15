53bb65cd309d242fd4c1818be0e3cc56548a2c69
Aphenphos <MaxwillWinters@gmail.com>
2022-08-15 14:06:32 -0400

    can create a new user

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { get } = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const fakeUser = {
   firstName: 'Mr',
@@ -19,6 +19,17 @@ describe('tests user routes', () => {
   it('returns the users', async () => {
     const resp = await request(app).get('/api/v1/users');
     expect(resp.status).toBe(401);
+  });
+  it('makes a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(fakeUser);
+    const { firstName, lastName, email } = fakeUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email
+    });
   });
   afterAll(() => {
     pool.end();
