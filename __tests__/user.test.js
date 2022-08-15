a8d17761fcf5573793670c8db73dec95df393bf2
Aphenphos <MaxwillWinters@gmail.com>
2022-08-15 13:56:21 -0400

    users page properly protected from unauth

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,14 +1,24 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const { get } = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const fakeUser = {
+  firstName: 'Mr',
+  lastName: 'Man',
+  email: 'mrman@man.com',
+  password: 'imtheman'
+};
+
+
+describe('tests user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('returns the users', () => {
-    expect(1).toEqual(1);
+  it('returns the users', async () => {
+    const resp = await request(app).get('/api/v1/users');
+    expect(resp.status).toBe(401);
   });
   afterAll(() => {
     pool.end();
